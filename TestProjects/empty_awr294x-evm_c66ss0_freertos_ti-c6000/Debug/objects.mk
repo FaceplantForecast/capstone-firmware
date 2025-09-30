d9d6674b4adfb46e7527cac12c5c8e36eca366d5
hessef <35468516+hessef@users.noreply.github.com>
2025-09-30 18:26:40 -0500

    successfully ran multicore code

---
+++ TestProjects/empty_awr294x-evm_c66ss0_freertos_ti-c6000/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lfreertos.awr294x.c66.ti-c6000.debug.lib -ldrivers.awr294x.c66.ti-c6000.debug.lib -llibc.a
+
